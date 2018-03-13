39c8881f92fb413fb4eff70aedf9d56567949f76
Darren <darren.burford@kantar.com>
2018-03-13 12:17:11 +0700

    Steps 3-6: derived classes

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+
+		StandardGradeBook (string name) : base(name)
+		{
+			Type = Enums.GradeBookType.Standard;
+		}
+    }
+
+
+}
